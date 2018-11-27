2eff6a52a291377c153e9534633be8af645c1cd6
Justin Hernandez <jhernandez2@atlassian.com>
2018-11-27 16:47:43 +0100

    add View type

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/lenslocked/views"
 )
+
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site</h1>")
+	err := homeView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>To get in touch please contact <a href=\"#\">test</a></h1>")
+	err := contactView.Template.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
