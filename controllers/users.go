768e2031954710cbf87c7c6f7f9ea5042690a87e
Justin Hernandez <jhernandez2@atlassian.com>
2018-11-27 21:53:58 +0100

    add gorilla schema

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/schema"
 	"github.com/lenslocked/views"
 )
 
@@ -25,6 +26,11 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create is used to process the submitted signup form
 // will create a new account
 //
@@ -33,6 +39,10 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, r.PostFormValue("email"))
-	fmt.Fprintf(w, r.PostFormValue("password"))
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
